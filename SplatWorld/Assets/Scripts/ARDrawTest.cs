7acbba47c2db35b62d4b3556e29ae433730fb03c
mmr6424 <mmr6424@rit.edu>
2023-09-06 14:05:27 -0400

    Specified Drawing to Walls
    Renamed ARPlaneCreation to ARDrawTest and converted the code to draw using Charles' drawing code, rather than simply place squares.

---
+++ SplatWorld/Assets/Scripts/ARDrawTest.cs
@@ -0,0 +1,191 @@
+// Copyright 2022 Niantic, Inc. All Rights Reserved.
+// This section of code is taken from Niantic's Hit Tester, and modified to draw continuously.
+using System;
+using System.Collections.Generic;
+
+using Niantic.ARDK.AR;
+using Niantic.ARDK.AR.ARSessionEventArgs;
+using Niantic.ARDK.AR.HitTest;
+using Niantic.ARDK.External;
+using Niantic.ARDK.Utilities;
+using Niantic.ARDK.Utilities.Input.Legacy;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class ARDrawTest : MonoBehaviour
+{
+    /// The camera used to render the scene. Used to get the center of the screen.
+    public Camera Camera;
+
+    /// The types of hit test results to filter against when performing a hit test.
+    [EnumFlagAttribute]
+    public ARHitTestResultType HitTestType = ARHitTestResultType.ExistingPlane;
+
+    /// Internal reference to the session, used to get the current frame to hit test against.
+    private IARSession _session;
+
+    private Mesh mesh;
+    private Vector3 lastMousePos;
+    public Vector3 hitPosition;
+    public float lineThickness;
+    public float minDistance;
+
+    private void Start()
+    {
+        lineThickness = 0.01f;
+        minDistance = 0.01f;
+        ARSessionFactory.SessionInitialized += OnAnyARSessionDidInitialize;
+    }
+
+    private void OnAnyARSessionDidInitialize(AnyARSessionInitializedArgs args)
+    {
+        _session = args.Session;
+        _session.Deinitialized += OnSessionDeinitialized;
+    }
+
+    private void OnSessionDeinitialized(ARSessionDeinitializedArgs args)
+    {
+    }
+
+    private void OnDestroy()
+    {
+        ARSessionFactory.SessionInitialized -= OnAnyARSessionDidInitialize;
+
+        _session = null;
+    }
+
+    private void Update()
+    {
+        if (_session == null)
+        {
+            return;
+        }
+
+        if (PlatformAgnosticInput.touchCount <= 0)
+        {
+            return;
+        }
+
+        var touch = PlatformAgnosticInput.GetTouch(0);
+        hitPosition = GetPosition(touch);
+
+        if (touch.phase == TouchPhase.Began)
+        {
+            Draw();
+        }
+        else if (touch.phase == TouchPhase.Moved) {
+            EndDraw();
+        }
+    }
+
+    private Vector3 GetPosition(Touch touch) {
+        var currentFrame = _session.CurrentFrame;
+        if (currentFrame == null)
+        {
+            return new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+        }
+
+        var results = currentFrame.HitTest
+        (
+            Camera.pixelWidth,
+            Camera.pixelHeight,
+            touch.position,
+            HitTestType
+        );
+
+        int count = results.Count;
+        Debug.Log("Hit test results: " + count);
+
+        if (count <= 0)
+            return new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+
+        // Get the closest result
+        var result = results[0];
+
+        return result.WorldTransform.ToPosition();
+    }
+
+    private void Draw()
+    {
+        mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[4];
+        Vector2[] uv = new Vector2[4];
+        int[] triangles = new int[6];
+
+        vertices[0] = hitPosition;
+        vertices[1] = hitPosition;
+        vertices[2] = hitPosition;
+        vertices[3] = hitPosition;
+
+        uv[0] = Vector2.zero;
+        uv[1] = Vector2.zero;
+        uv[2] = Vector2.zero;
+        uv[3] = Vector2.zero;
+
+        triangles[0] = 0;
+        triangles[1] = 3;
+        triangles[2] = 1;
+
+        triangles[3] = 1;
+        triangles[4] = 3;
+        triangles[5] = 2;
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+        mesh.MarkDynamic();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        lastMousePos = hitPosition;
+    }
+    private void EndDraw() {
+        if (Vector3.Distance(hitPosition, lastMousePos) > minDistance)
+        {
+            Vector3[] vertices = new Vector3[mesh.vertices.Length + 2];
+            Vector2[] uv = new Vector2[mesh.uv.Length + 2];
+            int[] triangles = new int[mesh.triangles.Length + 6];
+
+            mesh.vertices.CopyTo(vertices, 0);
+            mesh.uv.CopyTo(uv, 0);
+            mesh.triangles.CopyTo(triangles, 0);
+
+            int vIndex = vertices.Length - 4;
+            int vIndex0 = vIndex + 0;
+            int vIndex1 = vIndex + 1;
+            int vIndex2 = vIndex + 2;
+            int vIndex3 = vIndex + 3;
+
+            Vector3 mouseForwardVector = (hitPosition - lastMousePos).normalized;
+            Vector3 normal2D = new Vector3(0, 0, -1f);
+            Vector3 newVertexUp = hitPosition + Vector3.Cross(mouseForwardVector, normal2D) * lineThickness;
+            Vector3 newVertexDown = hitPosition + Vector3.Cross(mouseForwardVector, normal2D * -1f) * lineThickness;
+
+            //debugVisual1.position = newVertexUp;
+            //debugVisual2.position = newVertexDown;
+
+            vertices[vIndex2] = newVertexUp;
+            vertices[vIndex3] = newVertexDown;
+
+            uv[vIndex2] = Vector2.zero;
+            uv[vIndex3] = Vector2.zero;
+
+            int tIndex = triangles.Length - 6;
+
+            triangles[tIndex + 0] = vIndex0;
+            triangles[tIndex + 1] = vIndex2;
+            triangles[tIndex + 2] = vIndex1;
+
+            triangles[tIndex + 3] = vIndex1;
+            triangles[tIndex + 4] = vIndex2;
+            triangles[tIndex + 5] = vIndex3;
+
+            mesh.vertices = vertices;
+            mesh.uv = uv;
+            mesh.triangles = triangles;
+
+            lastMousePos = hitPosition;
+        }
+    }
+}
