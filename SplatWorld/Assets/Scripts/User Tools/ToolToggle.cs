871ee94dd386739b71205b3b11e892f9f093a1c1
thomasismartinez <98428471+thomasismartinez@users.noreply.github.com>
2024-02-02 21:31:22 -0500

    tool button shows selected tool

--- SplatWorld/Assets/Scripts/User Tools/ToolToggle.cs
+++ SplatWorld/Assets/Scripts/User Tools/ToolToggle.cs
@@ -1,17 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToolToggle : MonoBehaviour
 {
+    public Image toolButton;
     public GameObject brush, can, colorPicker;
     // Start is called before the first frame update
+
+    public Sprite brushSprite, canSprite, pencilSprite, bucketSprite, eraserSprite;
+
+    public void ActivatePencil()
+    {
+        brush.SetActive(false);
+        can.SetActive(false);
+        colorPicker.SetActive(false);
+        // replace tool button sprite
+        toolButton.GetComponent<Image>().sprite = pencilSprite;
+    }
+
+    public void ActivateBucket()
+    {
+        brush.SetActive(false);
+        can.SetActive(false);
+        colorPicker.SetActive(false);
+        // replace tool button sprite
+        toolButton.GetComponent<Image>().sprite = bucketSprite;
+    }
+
+    public void ActivateEraser()
+    {
+        brush.SetActive(false);
+        can.SetActive(false);
+        colorPicker.SetActive(false);
+        // replace tool button sprite
+        toolButton.GetComponent<Image>().sprite = eraserSprite;
+    }
 
     public void ActivateBrush()
     {
         brush.SetActive(true);
         can.SetActive(false);
         colorPicker.SetActive(false);
+        // replace tool button sprite
+        toolButton.GetComponent<Image>().sprite = brushSprite;
     }
 
     public void ActivateCan()
@@ -19,6 +52,7 @@ public class ToolToggle : MonoBehaviour
         brush.SetActive(false);
         can.SetActive(true);
         colorPicker.SetActive(false);
+        toolButton.GetComponent<Image>().sprite = canSprite;
     }
 
     public void ActivateColorPicker()
