82919016ded7fe9c56d1c5374ef85dbad78d4bca
Moss Limpert <70284036+MossLimpert@users.noreply.github.com>
2024-04-24 14:18:26 -0400

    remove bad and unused UI using statements

--- SplatWorld/Assets/Scripts/UI/Scroll Views/GalleryItem.cs
+++ SplatWorld/Assets/Scripts/UI/Scroll Views/GalleryItem.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
-using static UnityEditor.FilePathAttribute;
 using static UnityEngine.UI.Button;
 
 public class GalleryItem : MonoBehaviour
